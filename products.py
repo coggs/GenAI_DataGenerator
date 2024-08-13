121dfa2a86695e9a25a5397e084799a2d567bfc6
Stuart Coggins <develop@coggs.net>
2024-08-13 16:21:46 +1000

    Create products.py
    Products (for Building Materials project)

---
+++ products.py
@@ -0,0 +1,7 @@
+Product = [
+    {"product": "concrete", "weight": 800},
+    {"product": "bricks", "weight": 800},
+    {"product": "sand", "weight": 100},
+    {"product": "plasterboard", "weight": 50},
+    {"product": "pebbles", "weight": 50}
+]
